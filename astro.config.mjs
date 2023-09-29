2a6797ae528975747005438550d2e23574226da1
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-09-29 13:51:00 -0700

    chore(astro.config.mjs): add new integrations and a `site`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-    integrations: [tailwind()],
+    site: "https://www.jeffreyberry.dev",
+    integrations: [
+        sitemap(),
+        tailwind(),
+    ],
 });
