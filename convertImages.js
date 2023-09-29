c3ff8469bac0005ed444d0b92eabeb0208c2718e
Jeffrey Berry <jeffreyberry93@gmail.com>
2023-09-28 17:14:30 -0700

    feat(init): initialize with `astro`

--- convertImages.js
+++
@@ -1,9 +0,0 @@
-const imagemin = require('imagemin');
-const webp = require('imagemin-webp');
-
-imagemin(['./images/original/**/*.{jpeg,jpg,png}'], {
-    destination: './images/compressed',
-    plugins: [
-        webp({ quality: 50 }),
-    ],
-});
